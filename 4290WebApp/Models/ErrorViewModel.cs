c844777d932f5a25943dc5d2b794521e70804d33
BlankCanvasTS <93033104+BlankCanvasTS@users.noreply.github.com>
2023-03-08 13:29:30 -0800

    New Framework

---
+++ 4290WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _4290WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
