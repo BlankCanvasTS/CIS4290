6331b0816be3d994eb42ecafa9fbb7a782a16067
kemaapp <kma.uni.app@gmail.com>
2023-03-19 19:24:03 -0700

    Created Login Authen
    created Login Authen

---
+++ 4290WebApp/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace _4290WebApp.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
